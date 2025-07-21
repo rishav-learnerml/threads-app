f44c6d69d73b5d54efcf2e8d4a5a29e8e1b36a83
rishav_learnerml <rishavbio1@gmail.com>
2025-07-21 08:55:36 +0530

    Add initial styles, SVG icons, and prerendered routes for ThreadsApp
    - Created a new CSS file with base styles, theme, and utility classes.
    - Added main SVG icon and light variant for ThreadsApp.
    - Included a JSON file for prerendered routes, initializing with an empty routes object.

---
+++ dist/ThreadsApp/browser/chunk-A6QY23VF.js
@@ -0,0 +1,1 @@
+import{Ua as a,Va as n,bb as i,za as o}from"./chunk-OV4ECLEL.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(a(0,"p"),i(1,"about works!"),n())},encapsulation:2})};export{s as About};
